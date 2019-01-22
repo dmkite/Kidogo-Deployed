6d0086c65168a9a6820be6190e1fee682a7d9b6a
Dylan <kite.d92@gmail.com>
2019-01-22 13:37:38 -0800

    added padding to header

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './screens/HomeScreen'
 
-
-
 const AppNavigator = createStackNavigator({
   Home: { screen: HomeScreen }
 })
