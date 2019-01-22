4849a1e2f784d7e10ec2bdf4e72afb2cf41e681e
Dylan <kite.d92@gmail.com>
2019-01-22 13:48:25 -0800

    set up dashboard directory

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './screens/HomeScreen'
+import DashBoard from './screens/DashBoard';
 
 const AppNavigator = createStackNavigator({
-  Home: { screen: HomeScreen }
+  Home: { screen: HomeScreen },
+  Dash: {screen: DashBoard}
 })
 
 const AppContainer = createAppContainer(AppNavigator)
---
+++ components/DashView/styles.js
@@ -0,0 +1,5 @@
+export const styles = {
+  dash: {
+    height:200
+  }
+}
