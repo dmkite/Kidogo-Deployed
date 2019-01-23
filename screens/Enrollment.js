f5bd35828fa7a36ac522e7edd7233ece5ce4788e
Dylan <kite.d92@gmail.com>
2019-01-23 10:19:21 -0800

    setting up expo camera

--- screens/Enrollment.js
+++ screens/Enrollment.js
@@ -1,35 +1,63 @@
 import React, {Component} from 'react'
-import {View} from 'react-native'
-import {FormLabel, FormInput, FormValidationMessage, Button} from 'react-native-elements'
+import {View, Text, TouchableOpacity} from 'react-native'
 import { bindActionCreators } from 'redux';
 import Header from '../components/Header'
 import {connect} from 'react-redux' 
-import { RNCamera } from 'react-native-camera'
+import {Camera, Permissions} from 'expo'
+import {Icon} from 'react-native-elements'
 
 class Enrollment extends Component{
   constructor(props){
     super(props)
+    this.state = {
+      hasCameraPermission: null,
+      type: Camera.Constants.Type.back
+    }
   }
 
-  takePicture = async () => {
-    if(this.camera){
-      const options = { quality:0.5, base64: true}
-      const data = await this.camera.takePictureAsync(options)
-      console.log(data.uri)
-    }
-  
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
   }
 
   render(){
-    <View style={{flex:1}}>
-      <RNCamera
-        ref={ref => {this.camera = ref}}
-        style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}
-        type={RNCamera.Constants.Type.back}
-        permissionDialogTitle={'Can we use your camera?'}
-        permissionDialogMessage={'We need your permission to take a picture using your camera'}
-      />
-    </View>
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                
+                <Icon name="camera-alt" color="white" size={50}/>
+                
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
 
