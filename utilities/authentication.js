079b0c9b63362a9922a0e42ab662e2ef3daa5c6e
Dylan <kite.d92@gmail.com>
2019-02-17 14:38:55 -0800

    user can authorize and receive token

--- utilities/authentication.js
+++ utilities/authentication.js
@@ -1,68 +1,81 @@
 import { Auth } from 'aws-amplify';
+import {SecureStore} from 'expo'
 
-export const signIn = async (username, password) => {
-  try {
-    const user = await Auth.signIn(username, password);
-    if (user.challengeName === 'SMS_MFA' ||
-      user.challengeName === 'SOFTWARE_TOKEN_MFA') {
-      // You need to get the code from the UI inputs
-      // and then trigger the following function with a button click
-      const code = getCodeFromUserInput();
-      // If MFA is enabled, sign-in should be confirmed with the confirmation code
-      const loggedUser = await Auth.confirmSignIn(
-        user,   // Return object from Auth.signIn()
-        code,   // Confirmation code  
-        mfaType // MFA Type e.g. SMS, TOTP.
-      );
-    } else if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
-      const { requiredAttributes } = user.challengeParam; // the array of required attributes, e.g ['email', 'phone_number']
-      // You need to get the new password and required attributes from the UI inputs
-      // and then trigger the following function with a button click
-      // For example, the email and phone_number are required attributes
-      const { username, email, phone_number } = getInfoFromUserInput();
-      const loggedUser = await Auth.completeNewPassword(
-        user,               // the Cognito User Object
-        newPassword,       // the new password
-        // OPTIONAL, the required attributes
-        {
-          email,
-          phone_number,
-        }
-      );
-    } else if (user.challengeName === 'MFA_SETUP') {
-      // This happens when the MFA method is TOTP
-      // The user needs to setup the TOTP before using it
-      // More info please check the Enabling MFA part
-      Auth.setupTOTP(user);
-    } else {
-      // The user directly signs in
-      console.log(user) 
-    }
-  } catch (err) {
-    if (err.code === 'UserNotConfirmedException') {
-      // The error happens if the user didn't finish the confirmation step when signing up
-      // In this case you need to resend the code and confirm the user
-      // About how to resend the code and confirm the user, please check the signUp part
-    } else if (err.code === 'PasswordResetRequiredException') {
-      // The error happens when the password is reset in the Cognito console
-      // In this case you need to call forgotPassword to reset the password
-      // Please check the Forgot Password part.
-    } else {
-      console.log(err);
-    }
-  }
-  // For advanced usage
-  // You can pass an object which has the username, password and validationData which is sent to a PreAuthentication Lambda trigger
+// export const signIn = async (username, password) => {
+//   try {
+//     const user = await Auth.signIn(username, password);
+//     if (user.challengeName === 'SMS_MFA' ||
+//       user.challengeName === 'SOFTWARE_TOKEN_MFA') {
+//       // You need to get the code from the UI inputs
+//       // and then trigger the following function with a button click
+//       const code = getCodeFromUserInput();
+//       // If MFA is enabled, sign-in should be confirmed with the confirmation code
+//       const loggedUser = await Auth.confirmSignIn(
+//         user,   // Return object from Auth.signIn()
+//         code,   // Confirmation code  
+//         mfaType // MFA Type e.g. SMS, TOTP.
+//       );
+//     } else if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
+//       const { requiredAttributes } = user.challengeParam; // the array of required attributes, e.g ['email', 'phone_number']
+//       // You need to get the new password and required attributes from the UI inputs
+//       // and then trigger the following function with a button click
+//       // For example, the email and phone_number are required attributes
+//       const { username, email, phone_number } = getInfoFromUserInput();
+//       const loggedUser = await Auth.completeNewPassword(
+//         user,               // the Cognito User Object
+//         newPassword,       // the new password
+//         // OPTIONAL, the required attributes
+//         {
+//           email,
+//           phone_number,
+//         }
+//       );
+//     } else if (user.challengeName === 'MFA_SETUP') {
+//       // This happens when the MFA method is TOTP
+//       // The user needs to setup the TOTP before using it
+//       // More info please check the Enabling MFA part
+//       Auth.setupTOTP(user);
+//     } else {
+//       // The user directly signs in
+//       console.log(user) 
+//     }
+//   } catch (err) {
+//     if (err.code === 'UserNotConfirmedException') {
+//       // The error happens if the user didn't finish the confirmation step when signing up
+//       // In this case you need to resend the code and confirm the user
+//       // About how to resend the code and confirm the user, please check the signUp part
+//     } else if (err.code === 'PasswordResetRequiredException') {
+//       // The error happens when the password is reset in the Cognito console
+//       // In this case you need to call forgotPassword to reset the password
+//       // Please check the Forgot Password part.
+//     } else {
+//       console.log(err);
+//     }
+//   }
+//   // For advanced usage
+//   // You can pass an object which has the username, password and validationData which is sent to a PreAuthentication Lambda trigger
   
-  Auth.signIn({
+// }
+export const signIn = async (username, password, success, failure) => {
+  return Auth.signIn({ // ADDED RETURN 2/18
     username, // Required, the username
     password, // Optional, the password
-    validationData, // Optional, a random key-value pair map which can contain any key and will be passed to your PreAuthentication Lambda trigger as-is. It can be used to implement additional validations around authentication
-  }).then(user => console.log(user))
-    .catch(err => console.log(err));
+    // validationData, // Optional, a random key-value pair map which can contain any key and will be passed to your PreAuthentication Lambda trigger as-is. It can be used to implement additional validations around authentication
+  }).then(async user => {
+    console.log(user)
+    const tokenObj = {
+      accessToken: user.signInUserSession.accessToken.jwtToken,
+      idToken: user.signInUserSession.idToken.jwtToken,
+      refreshToken: user.signInUserSession.refreshToken.token,
+    }
+    await SecureStore.setItemAsync('_TOKEN', JSON.stringify(tokenObj))
+    return Promise.all([success(false, 'needSignIn'), success(false, 'loading'), failure('Authorization successful')])
+  })
+    .catch(err => {
+      console.log(err)
+      return failure('Sign in unsucessful')
+    });
 }
-
-
 
 export function signUp(username, password){
   Auth.signUp({
